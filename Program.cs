f9a7c840611f6034318b694db5632b7a29209d76
Bipeen <acharyab@seas.upenn.edu>
2019-04-12 07:09:17 -0700

    Initial check-in of module WorkingWithLINQ

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WorkingWithLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+
+            // Display each card generated and place in startingDeck in the console
+            foreach (var card in startingDeck)
+                Console.WriteLine(card);
+
+            // 52 cards in a deck, 52/2 = 26
+            var top = startingDeck.Take(26);
+            var bottom = startingDeck.Skip(26)
+        }
+
+
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+
+    }
+}
+
+namespace LinqFaroShuffle
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+
+        }
+    }
+}
